2241c737c043938020b7f1e6f6f6ff072aad0e34
Victor <victordanieleviz@gmail.com>
2023-11-22 07:40:28 -0300

    add form validation

---
+++ js/main.js
@@ -0,0 +1,61 @@
+let form = document.getElementById("form");
+
+let inputName = document.getElementById("name");
+let inputEmail = document.getElementById("email");
+let inputPhone = document.getElementById("phone")
+
+let emailRegex = new RegExp ( /^[a-zA-Z0-9._-]+@[a-zA-Z0-9._-]+\.[a-z]{2,}$/);
+let nameRegex = new RegExp ( /^[A-Za-záàâãéèêíïóôõöúçñÁÀÂÃÉÈÍÏÓÔÕÖÚÇÑ ]+$/);
+let phoneRegex = new RegExp ( /^[0-9]$/ );
+
+form.addEventListener("submit", (e) => {
+  e.preventDefault();
+
+  if (!isValidEmail()) return;
+  if (!isValidName()) return;
+  if (!isValidPhone()) return;
+
+  form.submit();
+})
+
+inputName.addEventListener("input", () => {
+  verifyInput("invalidName", isValidName);
+});
+
+inputEmail.addEventListener("input", () => {
+  verifyInput("invalidEmail", isValidEmail);
+});
+
+inputPhone.addEventListener("input", () => {
+  verifyInput("invalidPhone", isValidPhone);
+})
+
+function verifyInput (id, isValid ) {
+  message = document.getElementById(id);
+  if (isValid()){
+    message.style.display = "none";
+  } else {
+    message.style.display = "inline";
+  }
+}
+
+function isValidName() {
+  if (inputName.value.length == 0 || !nameRegex.test(inputName.value) ) {
+    return false;
+  }
+  return true;
+}
+
+function isValidEmail() {
+  if(inputEmail.value.length == 0 || !emailRegex.test(inputEmail.value)){
+    return false;
+  }
+  return true;
+}
+
+function isValidPhone() {
+  if (inputPhone.value.length < 8 || inputPhone.value < 0) {
+    return false;
+  }
+  return true;
+}
